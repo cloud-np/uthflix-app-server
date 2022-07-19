b27b8e2fdba9858e3ffc86ed11a0c3b68f11ebd8
cloud-np <cloud-np@outlook.com>
2022-07-19 18:07:30 +0300

    Fixed the repo because it was missing crucial tings to operate

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,9 +15,6 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
-$app->bind('path.public', function() {
-    return __DIR__;
-});
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
